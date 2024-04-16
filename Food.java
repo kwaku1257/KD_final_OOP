6ff1e4fe0b15c06e8d4e65d0bfd8d6799d33f3e9
Suzzie <129015477+Suzzie234@users.noreply.github.com>
2024-04-16 11:54:19 +0000

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,17 +1,19 @@
 public class Food{
-
+//Creating instance variables
     private String name;
   
     private double calorieCount;
    
     private String foodGroup;
 
+//Constructor
     public Food(String name, double calorieCount, String foodGroup){
         this.name = name;
         this.calorieCount = calorieCount;
         this.foodGroup = foodGroup;
     }
 
+//Accessor Methods
     public double getCalories(){
         return calorieCount;
     }
